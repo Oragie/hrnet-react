c1811d6b46e623e679e8294234bf28c8e0a9924c
Vinko <vinkoloic@gmail.com>
2025-05-26 13:29:25 +0200

    fix scss and structure

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,13 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import logo from "../../assets/img/logoHRNet_nobg.webp";
 import iconAdd from "../../assets/img/new-employee.png";
 import iconList from "../../assets/img/List.png";
 import "./_header.scss";
 
 function Header() {
+  const location = useLocation();
+  const isHome = location.pathname === "/";
+
   return (
     <header className="hrnet-header">
       <div className="hrnet-info">
@@ -15,14 +18,17 @@ function Header() {
           </NavLink>
         </div>
         <nav className="hrnet-nav">
-          <NavLink to="/" className="nav-button">
-            <img src={iconAdd} alt="Add" className="nav-icon" />
-            Add New Employee
-          </NavLink>
-          <NavLink to="/employees" className="nav-button">
-            <img src={iconList} alt="List" className="nav-icon" />
-            Employee List
-          </NavLink>
+          {isHome ? (
+            <NavLink to="/employees" className="nav-button">
+              <img src={iconList} alt="List" className="nav-icon" />
+              <span className="nav-text">Employee List</span>
+            </NavLink>
+          ) : (
+            <NavLink to="/" className="nav-button">
+              <img src={iconAdd} alt="Add" className="nav-icon" />
+              <span className="nav-text">Add New Employee</span>
+            </NavLink>
+          )}
         </nav>
       </div>
     </header>
